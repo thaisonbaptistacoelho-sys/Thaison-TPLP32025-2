da4f06a5d9161341c98d28eb6cba10a103c9250e
Thaison <thaisonbaptistacoelho@gmail.com>
2025-11-08 14:33:21 -0300

    Testando

---
+++ TrabalhoLP3/src/main/java/com/mycompany/trabalholp3/TrabalhoLP3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalholp3;
+
+/**
+ *
+ * @author Thaison
+ */
+public class TrabalhoLP3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
